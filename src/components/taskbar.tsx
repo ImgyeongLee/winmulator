76123c857f291742cfb66cea79994f471f701048
Hla Htun <kshap.karki@gmail.com>
2024-05-25 15:02:49 -0700

    fix: using Image from next/image instead of img

--- src/components/taskbar.tsx
+++ src/components/taskbar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Image from "next/image"
+
 interface TaskbarProps {
     theme: string;
 }
@@ -11,9 +13,11 @@ const Taskbar: React.FC<TaskbarProps> = ({theme}) => {
             className={`${position} text-center text-white flex flex-row justify-between items-center
                         bg-taskbar-gradient z-1`}
         >
-            <img
+            <Image
                 src="/windowsXP_taskbar.webp"
                 alt="windowsXP taskbar"
+                width={200}
+                height={400}
                 className="w-12 p-0 m-0 cursor-pointer hover:brightness-110 z-2"
             />
             <div>End setting</div>
