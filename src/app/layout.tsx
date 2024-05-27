267fac589135f0db62ae818ea3b57a1514343a6a
Hla Htun <kshap.karki@gmail.com>
2024-05-27 09:55:07 -0700

    fix: overflowing apps hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     return (
         <StoreProvider>
             <html lang="en">
-                <body className={xp.className}>{children}</body>
+                <body className={xp.className + ' overflow-x-hidden'}>{children}</body>
             </html>
         </StoreProvider>
     );
